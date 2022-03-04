54a12c52561ca34c555f8757d5dde2ffc0e8c4bf
Gabriel Carneiro <gabriel29music@gmail.com>
2022-03-04 11:14:48 -0300

    heroku config

--- src/db_connection.js
+++ src/db_connection.js
@@ -6,6 +6,9 @@ const knex = require("knex")({
       password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT,
       database: process.env.DB_DATABASE,
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
   });
   
