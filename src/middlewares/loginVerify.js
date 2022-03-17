739c84c7918eb362719f66ccc41e81717f476fd7
Gabriel Carneiro <gabriel29music@gmail.com>
2022-03-17 10:08:18 -0300

    login

---
+++ src/middlewares/loginVerify.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const knex = require("../db_connection");
+
+const loginVerify = async (req, res, next) => {
+
+    const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ mensagem: "não autorizado!!" });
+  }
+
+
+  try {
+    const token = authorization.replace('Bearer ', '').trim();
+
+    const { id } = jwt.verify(token, process.env.JWT_SECRET);
+
+    const findUser = await knex('users').where({ id }).first();
+
+    if (!findUser) {
+        return res.status(404).json({ mensagem: 'Usuario não encontrado' });
+    }
+
+    const { password:_, ...user } = findUser;
+
+    req.user = user;
+
+    next();
+      
+  } catch (error) {
+     return res.status(400).json({mensagem: error.message})
+  }
+};
+
+module.exports = {
+    loginVerify
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const { login, register } = require('./controllers/users');
 const { getPosts, sendPost, deletePost, editPost, getPost } = require('./controllers/posts');
+const { loginVerify } = require('./middlewares/loginVerify');
 const routes = express();
 
 routes.get('/posts', getPosts);
 routes.get('/post/:id', getPost);
+routes.post('/register', register)
+routes.post('/login', login);
+
+routes.use(loginVerify)
 routes.post('/posts', sendPost);
 routes.delete('/posts/:id', deletePost);
 routes.put('/posts/:id', editPost);
