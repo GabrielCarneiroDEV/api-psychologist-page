1245614366dc21d622519b14cd1bf326a6138c73
Gabriel Carneiro <gabriel29music@gmail.com>
2022-07-19 11:06:47 -0300

    envio de email configurado

---
+++ src/controllers/sendMail.js
@@ -0,0 +1,23 @@
+const sendMail = async (req, res) => {
+  const { name, email, phone, text } = req.body;
+
+  const message = `Nome: ${name}
+  E-mail: ${email}
+  Telefone: ${phone}
+
+  Mensagem: ${text}
+  ` 
+
+  try {
+    transporter.sendMail({
+        from: `Gabriel <${process.env.MAIL_USER}>`,
+        to: process.env.MAIL_TO,
+        subject: `Nova mensagem de ${name}`,
+        text: message,
+      });
+      return res.json("Mensagem enviada com sucesso");
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
